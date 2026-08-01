ed09d51fee9b81e672de7a428ef6a9bc307102c3
bobblet11 <benglover78@gmail.com>
2026-08-02 02:01:43 +0800

    fix: split linting rules into 2 for back and frontend

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "module"}},
+  { languageOptions: { globals: { ...globals.browser, process: "readonly" } }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
