53d9de7170c992a4704cb2dbf270732fdbbcbb2d
bobblet11 <benglover78@gmail.com>
2026-08-12 17:35:16 +0800

    fix: debugging cicd to see if .env show up in production

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Upload from "./pages/Upload";
 
 function App() {
+        useEffect(() => {
+                console.log("API URL:", process.env.REACT_APP_API_URL);
+                console.log("Feature flag:", process.env.REACT_APP_FEATURE_FLAG);
+        }, []);
+
         return (
                 <div>
                         <Router>
