4e338de694185a726490b722abf4ab4fa5de5a78
bobblet11 <benglover78@gmail.com>
2026-08-13 19:25:32 +0800

    refactor: frontend. moved files into proper files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,11 @@
-import "./App.css";
+import "./styles/App.css";
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Upload from "./pages/Upload";
+import reportWebVitals from "./reportWebVitals";
 
 function App() {
-        useEffect(() => {
-                console.log("API URL:", process.env.REACT_APP_API_URL);
-                console.log("Feature flag:", process.env.REACT_APP_FEATURE_FLAG);
-        }, []);
-
         return (
                 <div>
                         <Router>
