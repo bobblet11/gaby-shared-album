fec3d51e57e160a31f0b030894a9047e8dedc155
bobblet11 <benglover78@gmail.com>
2026-08-15 00:05:15 +0800

    fix: import path

--- scripts/test-postgres-connection.js
+++ scripts/test-postgres-connection.js
@@ -1,6 +1,6 @@
 // run: node scripts/test-db.js
 const path = require("path");
-const db = require(path.resolve(__dirname, "../backend/db"));
+const db = require(path.resolve(__dirname, "../backend/src/configs/db"));
 
 (async () => {
         try {
