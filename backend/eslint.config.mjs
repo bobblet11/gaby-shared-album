59420f3e982d2e3526b9675830d2d9d40d2a9471
bobblet11 <benglover78@gmail.com>
2026-08-15 01:52:13 +0800

    fix: lint config

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -4,15 +4,14 @@ import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+        { ignores: ["src/libs/**", "tests/**"] },
         { files: ["**/*.{js,mjs,cjs,jsx}"] },
         { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
         { languageOptions: { globals: globals.node } },
         pluginJs.configs.recommended,
         pluginReact.configs.flat.recommended,
         {
-                ignores: ["src/libs/**"],
                 rules: {
-                        // ✅ Ignore unused "next" in Express handlers
                         "no-unused-vars": ["error", { argsIgnorePattern: "^next$" }],
                 },
         },
