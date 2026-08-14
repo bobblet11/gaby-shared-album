2b47baebab9d3fd339f06f8e23c34252ad6dc0f7
bobblet11 <benglover78@gmail.com>
2026-08-15 01:34:51 +0800

    fix: lint

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 const { TransactionError } = require("../utils/AppError");
 
-const errorHandler = (err, req, res, next) => {
+const errorHandler = (err, req, res) => {
         console.error("=== ERROR HANDLER CAUGHT ===");
         let appError = err;
         let transactionError = null;
