10a3e81023310762aa32faa68be9df6a191be72c
bobblet11 <benglover78@gmail.com>
2026-08-19 16:36:03 +0800

    add this to last commit too

---
+++ backend/src/configs/winston.js
@@ -0,0 +1,13 @@
+// logger.js
+const winston = require("winston");
+
+const logger = winston.createLogger({
+        level: "info",
+        format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+        transports: [
+                new winston.transports.Console(), // logs to console
+                new winston.transports.File({ filename: "app.log" }), // logs to file
+        ],
+});
+
+module.exports = logger;
