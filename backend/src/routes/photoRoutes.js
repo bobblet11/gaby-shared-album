42287727325b7c86beb33b7cbe88e1fcd2cca98e
bobblet11 <benglover78@gmail.com>
2026-09-06 20:54:43 +0800

    feat: new metrics, decoupled middleware to allow for custom statsd keys

--- backend/src/routes/photoRoutes.js
+++ backend/src/routes/photoRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const upload = require("../configs/multer")
+const upload = require("../configs/multer");
 const { getAllPhotos, getPhotoById, uploadPhoto, deletePhoto, editPhoto } = require("../controllers/photoController");
+const recordMetrics = require("../middlewares/recordMetrics");
 
 const router = express.Router();
 
-
-router.get("/", getAllPhotos);
-router.get("/:id", getPhotoById);
-router.post("/", upload.array("image"), uploadPhoto);
-router.delete("/:filename", deletePhoto);
-router.put("/:filename", editPhoto)
+router.get("/", recordMetrics("photos.getAll"), getAllPhotos);
+router.get("/:id", recordMetrics("photos.getById"), getPhotoById);
+router.post("/", recordMetrics("photos.upload"), upload.array("image"), uploadPhoto);
+router.delete("/:filename", recordMetrics("photos.delete"), deletePhoto);
+router.put("/:filename", recordMetrics("photos.edit"), editPhoto);
 
 module.exports = router;
